89fc318cfa75fcd348bf02395a8ba7ee6d497e13
bewe <93899302+bernarduswillson@users.noreply.github.com>
2024-02-05 23:56:41 +0700

    fix: mobile sidebar backdrop blur bug

--- src/components/Navigation/Sidebar.tsx
+++ src/components/Navigation/Sidebar.tsx
@@ -3,13 +3,14 @@
 // Imports
 import React, { useState, useEffect } from 'react';
 import { motion, useScroll, useSpring } from 'framer-motion';
-import Image from 'next/image';
+import { useTheme } from 'next-themes';
 
 // Utils
 import { scrollToSection } from '@/lib/scroll';
 
 // Assets
-import RightArrow from '@/components/Icon/Navbar/RightArrow.svg';
+import RightArrow from '@/components/Icon/Navbar/RightArrow';
+import Close from '@/components/Icon/Navbar/Close';
 
 interface Menu {
   text: string;
@@ -55,43 +56,63 @@ const Sidebar = (): JSX.Element => {
   });
 
   // Mobile menu handler
-  const [isOpened, setIsOpened] = useState(false);
+  const [isOpened, setIsOpened] = useState(true);
+
+  // Theme handler
+  const { setTheme, resolvedTheme } = useTheme();
+  const [loading, setLoading] = useState(true);
+  const [mode, setMode] = useState(resolvedTheme);
+  
+  useEffect(() => {
+    setMode(resolvedTheme);
+    setLoading(false);
+  }, [resolvedTheme]);
 
   return (
     <aside className="fixed h-screen items-center z-50 flex">
       {/* Mobile */}
       <div
         className={`lg:hidden cursor-pointer ${isOpened ? 'hidden' : 'block'}`}
-        style={{ paddingLeft: '20px', paddingTop: '20px', paddingBottom: '20px', paddingRight: '10px'}}
+        style={{ paddingLeft: '5px', paddingTop: '20px', paddingBottom: '20px', paddingRight: '10px'}}
         onClick={() => setIsOpened(!isOpened)}
       >
-        <Image src={RightArrow} alt="Right Arrow" />
+        <RightArrow width={25} height={25} fillColor={`${mode == 'dark' && !loading ? 'white' : 'black'}`} />
       </div>
-      <div className={`fixed lg:hidden top-0 left-0 w-full h-full z-[60] transform transition-all duration-500 ease-in-out ${isOpened ? 'translate-x-0' : '-translate-x-full '}`} style={{ backdropFilter: 'blur(15px)' }}>
-        <div>
-          <div className="flex justify-end cursor-pointer" style={{ paddingLeft: '20px', paddingTop: '20px', paddingBottom: '20px', paddingRight: '20px'}}>
-            <div onClick={() => setIsOpened(!isOpened)}>
-              <Image src={RightArrow} alt="Right Arrow" className='rotate-180' />
+      <div className={`flex items-center justify-center fixed lg:hidden top-0 left-0 w-full h-full z-[60] transform transition-all duration-500 ease-in-out filter backdrop-blur-[10px] ${isOpened ? 'translate-x-0' : '-translate-x-full '}`} style={{ WebkitBackdropFilter: 'blur(10px)', backdropFilter: 'blur(10px)' }}>
+        <div className='flex items-center justify-center'>
+          <div className="h-fit relative flex">
+            <div className="w-[1px] bg-primary opacity-60"></div>
+            <motion.div
+              className="w-[5px] bg-primary"
+              style={{ scaleY, transformOrigin: 'top', marginTop: '56px', translateX: '-58%' }}
+            />
+            <div className="flex flex-col items-start text-md relative">
+              {MENU_LIST.map((menu: Menu) => (
+                <div
+                  key={menu.text}
+                  className={`flex relative cursor-pointer transition duration-300 ${
+                    hoveredSection === menu.section ? 'hover:bg-primary hover:text-secondary' : ''
+                  }`}
+                  onClick={() => scrollToSection(menu.section)}
+                  onMouseEnter={() => setHoveredSection(menu.section)}
+                  onMouseLeave={() => setHoveredSection(null)}
+                >
+                  <div
+                    className={`${menu.text === '01' ? '' : 'hidden'} absolute top-0 left-0 bg-primary w-[5px] h-[56px] translate-x-[-7.9px]`}
+                  ></div>
+                  <div className="my-4 px-3">{menu.text}</div>
+                  <div className='my-4 px-3 font-bold' onClick={() => setIsOpened(false)}>{menu.section}</div>
+                </div>
+              ))}
             </div>
           </div>
         </div>
-        <div className="flex flex-col h-full w-full" style={{ paddingLeft: '15vw', paddingTop: '15vh' }}>
-          <div className='font-bold text-2xl'>
-            Menu
-          </div>
-          <div className='bg-primary' style={{ height: '3px', width: '40vw'}}></div>
-          {MENU_LIST.map((menu: Menu) => (
-            <div
-              key={menu.text}
-              className="flex relative cursor-pointer transition duration-300"
-              onClick={() => {
-                scrollToSection(menu.section);
-                setIsOpened(!isOpened);
-              }}
-            >
-              <div className="my-4 px-3">{menu.section}</div>
+        <div>
+          <div className="absolute top-0 right-0 cursor-pointer" style={{ paddingLeft: '20px', paddingTop: '20px', paddingBottom: '20px', paddingRight: '20px'}}>
+            <div onClick={() => setIsOpened(!isOpened)} className={`transform duration-500 ${isOpened ? 'rotate-0' : 'rotate-90'}`}> 
+              <Close width={25} height={25} fillColor={`${mode === 'dark' && !loading ? 'white' : 'black'}`} />
             </div>
-          ))}
+          </div>
         </div>
       </div>
       
